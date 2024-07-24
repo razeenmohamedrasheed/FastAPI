ab81cf43a1f9e1e9db1be9c39a09e3b36ecc7367
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-07-24 19:41:41 +0530

    Crud Operations

--- Products/main.py
+++ Products/main.py
@@ -17,6 +17,7 @@ def get_db():
     finally:
         db.close()
 
+# Add a product
 @app.post('/products')
 def addProduct(requestData:schemas.Product, db:Session = Depends(get_db)):
     new_product = Models.product(
@@ -31,7 +32,7 @@ def addProduct(requestData:schemas.Product, db:Session = Depends(get_db)):
         "message":"product added successFully",
         "data":requestData
     }
-
+#List all products
 @app.get('/products')
 def getAllProducts(db:Session = Depends(get_db)):
     all_products = db.query(Models.product).all()
@@ -40,10 +41,35 @@ def getAllProducts(db:Session = Depends(get_db)):
           "data":all_products
     }
 
+#Filter individual products
 @app.get('/products/{id}')
-def getAllProducts(id,db:Session = Depends(get_db)):
+def getIndividualProducts(id,db:Session = Depends(get_db)):
     products = db.query(Models.product).filter(Models.product.id == id).first()
     return{
           "message":"Listed Successfully",
           "data":products
+    }
+
+#Delet products
+
+@app.delete('/products/{id}')
+def deleteProducts(id,db:Session = Depends(get_db)):
+    db.query(Models.product).filter(Models.product.id == id).delete(synchronize_session=False)
+    db.commit()
+    return{
+          "message":"Deleted Successfully",
+    }
+
+@app.put('/products/{id}')
+def editProduct(id,requestData:schemas.Product, db:Session = Depends(get_db)):
+    products = db.query(Models.product).filter(Models.product.id == id)
+    if not products.first():
+        pass
+
+    products.update(requestData.dict())
+    db.commit()
+
+    return{
+        "message":"product added successFully",
+        "data":requestData
     }
