a10726d429807a4e9a20af741bd565d226e4d9c0
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-07-28 17:01:09 +0530

    JWT updates

--- Products/routers/login.py
+++ Products/routers/login.py
@@ -4,12 +4,28 @@ from ..database import get_db
 from fastapi.params import Depends
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from datetime import datetime,timedelta,timezone
+from jose import jwt,JWTError
+from fastapi.security import OAuth2PasswordBearer
 
 router = APIRouter(
     tags=['Login']
 )
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+OAuth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+SECRET_KEY = "fJ5z1iUGQCi2gL0tSQhCwBwlC0uOJBN023Bwy8Z6Y79fkFSGoEveCsXP3UNJP0yXjsWmxNGwy42mzKAU8Anidz6ZfLpPBwn0o6WK"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 20
+
+# TOKEN GENERATION LOGIC
+def generate_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    encoded_JWT = jwt.encode(to_encode,SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_JWT
 
 @router.post('/login')
 def createUser(payload:schemas.Login,db: Session = Depends(get_db)):
@@ -18,4 +34,23 @@ def createUser(payload:schemas.Login,db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User Not Found")
     if not pwd_context.verify(payload.password,user.password):
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid Password")
-    return payload
+    access_token = generate_token(data={"sub":user.username})
+    return{
+        "accesstoken":access_token,
+        "tokentype":"bearer"
+    }
+    
+def getCurrentUser(token:str = Depends(OAuth2_scheme)):
+    credential_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid Credentials",
+        headers={'WWW-Authenticate':'Bearer'}
+    )
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username:str = payload.get('sub')
+        if username is None:
+            raise credential_exception
+        token_data = schemas.TokenData
+    except JWTError:
+        raise credential_exception
